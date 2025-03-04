3c38193fe7319cf2de8fdd7f7ecf08c50131188f
Leonid Petrov <lvpjsdev@gmail.com>
2025-03-05 00:31:28 +0300

    feat: implement api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { useQuery } from '@tanstack/react-query';
+import { fetchMatches } from './api/api';
+import { Loader } from './components/Loader';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isFetching, isError } = useQuery({
+    queryKey: ['matches'],
+    queryFn: fetchMatches,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isFetching && <Loader />}
+      <pre>{data && JSON.stringify(data, null, 2)}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

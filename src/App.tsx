6e41d7e65e46f8325e34bf1345dde171b4dd82dc
Leonid Petrov <lvpjsdev@gmail.com>
2025-03-05 00:32:46 +0300

    refactor: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { useQuery } from '@tanstack/react-query';
 import { fetchMatches } from './api/api';
 import { Loader } from './components/Loader';

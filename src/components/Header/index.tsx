6a834b47dafc4daa5e14fad88cd996adac629eee
Leonid Petrov <lvpjsdev@gmail.com>
2025-03-05 11:38:36 +0300

    fix: style header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { ErrorComponent } from '../ErrorComponent';
 import { Button } from '../Button';
 import RetryIcon from '../../assets/Refresh.svg?react';
+import logo from '../../assets/Logo.png';
+import styled from 'styled-components';
 
 interface Props {
   isError?: boolean;
@@ -9,10 +11,18 @@ interface Props {
   onRetry: () => void;
 }
 
+const StyledHeader = styled.header`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 export const Header: FC<Props> = ({ isError, isDisabled, onRetry }) => {
   return (
-    <header>
-      <h1>Match Tracker</h1>
+    <StyledHeader>
+      <img height={32} width={257} src={logo} />
       <div>
         {isError && <ErrorComponent />}
         <Button
@@ -23,6 +33,6 @@ export const Header: FC<Props> = ({ isError, isDisabled, onRetry }) => {
           Обновить
         </Button>
       </div>
-    </header>
+    </StyledHeader>
   );
 };
